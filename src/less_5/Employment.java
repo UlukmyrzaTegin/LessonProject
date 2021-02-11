ac5a72a4d52a8a9528b82368143158e981ba7db5
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-02-11 23:12:53 +0600

    Обновлен класс Employment

--- src/less_5/Employment.java
+++ src/less_5/Employment.java
@@ -9,14 +9,34 @@ import java.util.Scanner;
 public class Employment {
     public static void main(String[] args) {
         Scanner scanEmp = new Scanner(System.in);
-        System.out.println("Направление: ");
+        System.out.print("Направление: ");
         String direction = scanEmp.nextLine();
 
-        System.out.println("Зарплата: ");
+        System.out.print("Зарплата: ");
         int salary = scanEmp.nextInt();
 
-        System.out.println("Test");
+        System.out.print("Официональное трудоустройство? ");
+        boolean isOfficial = scanEmp.nextBoolean();
 
+        System.out.print("Есть переезд? ");
+        boolean isReplaceNeed = scanEmp.nextBoolean();
 
+        System.out.print("Начало рабочего дня: ");
+        int startHour = scanEmp.nextInt();
+
+        System.out.print("Конец рабочего дня: ");
+        int endHour = scanEmp.nextInt();
+
+        System.out.print("Необходимый стаж работы: ");
+        double exp = scanEmp.nextDouble();
+
+        System.out.print("Стек технологий: ");
+        String lang = scanEmp.next();
+
+        System.out.print("Карьерный рост? ");
+        boolean isCareer = scanEmp.nextBoolean();
+
+        boolean resultEmp =((direction.equals("bacend") && (salary >= 20000 || isOfficial) && exp <=2 && lang.equals("java")) || (isReplaceNeed && (startHour >= 9 && endHour <= 18))) && isCareer;
+        System.out.println(resultEmp);
     }
 }
