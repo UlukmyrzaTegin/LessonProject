be1d08a75bacbf427d87d1940f69c5f16d122c01
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-02-12 18:52:30 +0600

    Рефакторинг класс Arif

--- src/less_3/Arif.java
+++ src/less_3/Arif.java
@@ -8,15 +8,17 @@ public class Arif {
     public static void main(String[] args) {
         int a = 141;
         int b = 1354;
-
         //double c = (double)a / (double)b;
-
         int c = a % b;
-
         int i = 10;
         int d = ++a; //
+        int e = a + 5 * ++b;  //
+        int f = (a + 5) * ++b;
+        float g = a / b * i;
+        System.out.println("Резульатат: " + c);
+        System.out.println("Резульатат: " + e);
+        System.out.println("Резульатат: " + f);
+        System.out.println("Резульатат: " + g);
 
-
-        System.out.println(c);
     }
 }
