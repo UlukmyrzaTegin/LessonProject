0368dcc0696ea819984b56c3fe130cd5f94de07c
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-02-20 17:57:19 +0600

    Выполнен задача "Числы которые делятся на 3 без остатки", класс WithoutRemainder

---
+++ src/less_7_loops/homeWork/WithoutRemainder.java
@@ -0,0 +1,13 @@
+package less_7_loops.homeWork;
+
+/**
+ * TheSusanin
+ * 2/20/2021 Feb 2021
+ */
+public class WithoutRemainder {
+    public static void main(String[] args) {
+        for (int i = 3; i < 500 ; i+=3) {
+                System.out.println("Следующие числа делятся на 3 без остатки: " + i);
+        }
+    }
+}
