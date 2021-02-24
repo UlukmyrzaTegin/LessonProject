d7519969b5a88eea69e924b3c317dac99cb5e589
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-02-24 16:56:28 +0600

    1. Создать массив из 10 элементов типа int. Инициализировать каждый элемент рандомным числом от 1 до 10; 2. Найти максимальное число в этом массиве 3. Найти минимальное число 4. Найти среднее арифметическое 5. Создать другой массив и записать туда только четные числа из первого массива 6. Найти сумму всех элементов первого массива 8. Каждый третий элемент первого массива умножить 	на значение следующего элемента

--- src/less_8_array/homeWork/RandomArrays.java
+++ src/less_8_array/homeWork/RandomArrays.java
@@ -9,12 +9,16 @@ import java.util.Arrays;
 public class RandomArrays {
     public static void main(String[] args) {
         int[] array = new int[10];
+        int evenNum = 0, element = 10;
         int max = array[0], min = array[0];
-        int totalArray = 0, element = 10;
-        int averageArray = 0, sum1 = 0, sum2 = 0, sum3 = 0, chislo = array[0];
+        int totalArray = 0;
+        int averageArray = 0, sum1 = 0, sum2 = 0, sum3 = 0;
         for (int i = 0; i < array.length ; i++) {
             array[i] = (int) (Math.random() * (99-2) + 1) + 2;
             System.out.print(array[i] + "  ");
+            if (array[i] % 2 == 0){
+                evenNum++;
+            }
 
             if (array[i] > max) // мах число
                 max = array[i];
@@ -28,13 +32,24 @@ public class RandomArrays {
             sum3 = array[8] * array[9];
 
         }
-            System.out.println();
-            int[] arrayChet = new int[element];
-            for (int j = 0; j < array.length; j++) {
-                if (array[j] % 2 == 0)
-                    arrayChet[j] = array[j];
-                System.out.print(arrayChet[j] + "  ");
+        int[] array2 = new int[evenNum];
+        int index = 0;
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i] % 2 == 0){
+                array2[index] = array[i];
+                index++;
+            }
         }
+        System.out.println();
+        System.out.println(Arrays.toString(array2)); // вывожу четные числа в первом массиве
+
+//            System.out.println();
+//            int[] arrayChet = new int[element];
+//            for (int j = 0; j < array.length; j++) {
+//                if (array[j] % 2 == 0)
+//                    arrayChet[j] = array[j];
+//                System.out.print(arrayChet[j] + "  ");
+//        }
         System.out.println("\n Максимальное значение в массиве: " + max);
         System.out.println("Минимальное значение в массиве: " + min);
         System.out.println("Среднее арифметическое массива: " + averageArray);
