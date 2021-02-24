50499fc3a073519e167d353b5def9c09d0dcd2b4
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-02-24 16:32:57 +0600

    Выполнена задание: дан массив [12,4,3,5,6] -> перемешать элементы и записать в другой массив, класс CopyArrays

--- src/less_8_array/homeWork/CopyArrays.java
+++ src/less_8_array/homeWork/CopyArrays.java
@@ -6,7 +6,7 @@ package less_8_array.homeWork;
  */
 public class CopyArrays {
     public static void main(String[] args) {
-       int[] arr1 = new int[] {1, 2, 3, 4, 5, 6};
+       int[] arr1 = new int[] {3, 2, 5, 7, 9, 4};
        int[] arr2 = new int[arr1.length];
         for (int i = 0; i < arr1.length ; i++) {
             arr2[i] = arr1[i];
@@ -16,8 +16,8 @@ public class CopyArrays {
             System.out.print(arr1[i] + ", ");
         }
         System.out.println();
-        System.out.println("Элементы нового массива: ");
-        for (int i = 0; i < arr2.length ; i++) {
+        System.out.println("Массив в обратном порядке: ");
+        for (int i = arr1.length-1; i >= 0; i--) {
             System.out.print(arr2[i] + ", ");
         }
     }
