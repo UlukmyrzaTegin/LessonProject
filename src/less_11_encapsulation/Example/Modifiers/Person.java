93e84da064609a320b69b60889463229ddfa59ac
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-03-03 11:11:08 +0600

    Упражнения: Модификаторы

---
+++ src/less_11_encapsulation/Example/Modifiers/Person.java
@@ -0,0 +1,32 @@
+package less_11_encapsulation.Example.Modifiers;
+
+/**
+ * TheSusanin
+ * 03 март 2021
+ */
+public class Person {
+    String name;
+    protected  int age;
+    public String address;
+    private String phone;
+
+    public Person(String name, int age, String address, String phone) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public void displayName() {
+        System.out.printf("Name: %s \n", name);
+    }
+    public void displayAge() {
+        System.out.printf("Age: %d \n", age);
+    }
+    public void displayAddress() {
+        System.out.printf("Address: %s \n", address);
+    }
+    public void displayPhone() {
+        System.out.printf("Phone: %s \n" , phone);
+    }
+}
---
+++ src/less_11_encapsulation/Example/Modifiers/PersonModifiers.java
@@ -0,0 +1,24 @@
+package less_11_encapsulation.Example.Modifiers;
+
+/**
+ * TheSusanin
+ * 03 март 2021
+ */
+public class PersonModifiers {
+    public static void main(String[] args) {
+        Person ulukmyrza = new Person("Ulukimyrza", 30, "Baker Street", "+996709858858");
+        ulukmyrza.displayName();
+        ulukmyrza.displayAge();
+        ulukmyrza.displayAddress();
+        ulukmyrza.displayPhone();
+
+        System.out.println("-------------------------------");
+
+        System.out.println(ulukmyrza.name);
+        System.out.println(ulukmyrza.address);
+        System.out.println(ulukmyrza.age);
+        //System.out.println(ulukmyrza.phone); // ошибка, модификатор private
+
+    }
+
+}
