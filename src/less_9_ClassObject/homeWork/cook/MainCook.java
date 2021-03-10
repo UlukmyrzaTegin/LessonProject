f30be1e13f7543a59ce79eb9353b8a2c6df2997d
UlukmyrzaTegin <ulukmyrza.zhakypbekov@gmail.com>
2021-03-10 21:38:23 +0600

    Выполнен задание про Повар и был изменен классы MainCook и Cook

--- src/less_9_ClassObject/homeWork/cook/MainCook.java
+++ src/less_9_ClassObject/homeWork/cook/MainCook.java
@@ -8,12 +8,7 @@ import less_9_ClassObject.homeWork.cook.models.Cook;
  */
 public class MainCook {
     public static void main(String[] args) {
-        Cook cook = new Cook("Жаныбек", 32);
-      //  Scanner scan = new Scanner(System.in);
-      //  System.out.println("Выберите блюда: 1.Глазунья 2.Омлет");
-      //  cook.cookMethod(scan.next());
-//        cook.cookMethod("Глазунья");
-//        System.out.println("-----------------------------");
-//        cook.cookMethod("Омлет");
+        Cook cooker = new Cook("Жаныбек", 32);
+        cooker.cook("Омлет");
     }
 }
--- src/less_9_ClassObject/homeWork/cook/models/Cook.java
+++ src/less_9_ClassObject/homeWork/cook/models/Cook.java
@@ -5,77 +5,71 @@ package less_9_ClassObject.homeWork.cook.models;
  * 04 март 2021
  */
 public class Cook {
-    private String name;
-    private int age;
+     String fullName;
+     int age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Cook(String name, int age) {
-        this.name = name;
+    public Cook(String fullName, int age) {
+        this.fullName = fullName;
         this.age = age;
     }
 
     public Cook() {
     }
-
     private Product[] getProduct(String name) {
         return null;
 
     }
 
-    private void breakEggs() {   //разбить яйца
-
-    }
-
     public Dish cook(String name) {
+        System.out.println(fullName + " " + age + " года");
         Product[] products = getProduct(name);
-
-        if (name.equals("Омлет")) {
+        System.out.println();
+        if (name.equals("Глазунья")) {
             breakEggs();
+            vylojitNaSkovorodku();
+            posolit();
+            popershit();
+            jarit();
+        } else if (name.equals("Омлет")){
+            breakEggs();
+            shuffle();
+            addMilk();
+            vylojitNaSkovorodku();
+            posolit();
+            popershit();
+            jarit();
+        } else {
+            System.out.println("Я не умею готовить " + name);
         }
         return new Dish();
+    }
+    class Product{
 
     }
+    class Dish{
 
-//    public void cookMethod(String menu){
-//        System.out.println(name + " " + age + " года");
-//        switch (menu) {
-//            case "Глазунья":
-//                System.out.println("Глазунья:");
-//                System.out.println("Разбить яйца");
-//                System.out.println("Выложить на скороводку");
-//                System.out.println("Посолить");
-//                System.out.println("Поперчить");
-//                System.out.println("Жарить");
-//                break;
-//            case "Омлет:":
-//                System.out.println("Омлет:");
-//                System.out.println("Разбить яйца");
-//                System.out.println("Перемещать");
-//                System.out.println("Добавить молоко");
-//                System.out.println("Выложить на скороводку");
-//                System.out.println("Посолить");
-//                System.out.println("Поперчить");
-//                System.out.println("Жарить");
-//                break;
-//            default:
-//                System.out.println("Я не умею готовить " + menu);
-//                break;
-//        }
-        //   }
+    }
+    private void breakEggs() {   //разбить яйца
+        System.out.println("Разбить яйца");
+    }
+    private void vylojitNaSkovorodku(){
+        System.out.println("Выложить на сковородку");
+    }
+    private void posolit(){
+        System.out.println("Посолить");
+    }
+    private void popershit(){
+        System.out.println("Поперчить");
+    }
+    private void jarit(){
+        System.out.println("Жарить");
+    }
+
+    private void shuffle() {
+        System.out.println("Перемещать");
+    }
+    private void addMilk(){
+        System.out.println("Добавить молоко");
+    }
 }
 
